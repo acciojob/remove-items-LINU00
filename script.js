ca6b12c473e7a85ebc3afb576588c1d03b8ad03e
Aditya Singh <103933450+LINU00@users.noreply.github.com>
2025-09-15 02:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const button = document.querySelector('input[type="button"]')
+const select = document.getElementById('colorSelect')
+
+button.onclick = function() {
+    if (select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex)
+    }
+}
